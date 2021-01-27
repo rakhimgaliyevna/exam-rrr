b3a50cb9b4da432032048ac0601973e03f92c284
exam-ddd <you@example.com>
2021-01-27 19:41:53 +0600

    first

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styleE.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+</head>
+<body>
+    <div class="big">
+        <header>
+            <div class="main">
+                <a href="project/contact.html" class="ma_in">+7(123)456-78-90 </a>
+                <a href="project/contact.html" class="ma_in">| Доставка 24/7 | 9.00-18.00</a>
+            </div>
+            <div class="mini">
+                <a href="project/contact.html" class="ma_in">Войти/Регистрация</a>
+            </div>
+        </header>
+        <nav>
+            <h2>BALINA LUXE SHAR</h2>
+            <div class="menu">
+                <a href="project/news.html" class="me_nu">Новости</a>
+                <a href="project/katalog.html" class="me_nu">Новые поступления</a>
+                <a href="project/otzyvy.html" class="me_nu">Отзывы</a>
+                <a href="project/contact.html" class="me_nu">Контакты</a>
+                <a href="project/karta.html" class="me_nu">Карта</a>
+                <a href="index.php" class="me_nu">На главную</a>
+            </div>
+        </nav>
+        <footer>
+            <div class="logo">
+                <h1>BALINA LUXE SHAR</h1>
+                <hr class="hrlogo" align="center" width="180vh" size="3" color="#fff" />
+                <h2>ALMATY</h2>
+            </div>
+        </footer>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/EdAo9XYo1Zg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="video wow slideInLeft" data-wow-duration="2s"></iframe>
+        <section>
+            <div class="logo_mini">
+                <p class="logo_bottom">BALINA LUXE SHAR</p>
+                <hr class="hrlogo_mini" align="center" width="100vh" size="3" color="#fff" />
+                <p class="logo_bottom">ALMATY</p>
+            </div>
+            <div class="menu_mini">
+                <a href="project/news.html" class="mi_ni">Новости</a>
+                <a href="project/katalog.html" class="mi_ni">Новые поступления</a>
+                <a href="project/otzyvy.html" class="mi_ni">Отзывы</a>
+                <a href="project/contact.html" class="mi_ni">Контакты</a>
+                <a href="project/karta.html" class="mi_ni">Карта</a>
+            </div>
+            <div class="adress_mini">
+                <p class="ad_ress">г.Алматы, мкр.Алатау,</p>
+                <p class="ad_ress">ул.Жетбаева 32</p>
+                <div class="wa_ins_mini">
+                    <a href="" class="wa__ins"><img src="img/new/logoWA.jpg" alt="WA" width="40vh" height="40vh"></a>
+                    <a href="" class="wa__ins"><img src="img/new/logo100.jpg" alt="in100" width="40vh" height="40vh"></a>
+                </div>
+            </div>
+        </section>
+    </div>   
+    <script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    
+</body>
+</html>
